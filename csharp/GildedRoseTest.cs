0351cc4fea6dcc1da7c6b53d5b366652ce7d15f1
Eduardo Palacios <empalacios@ing.usac.edu.gt>
2019-10-24 01:13:31 -0600

    Add test for quality decrease when the date for sale is over.
    This tests decreasing the quality by 2 points when the sale date is
    over.

--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -26,5 +26,17 @@ namespace csharp
             Assert.AreEqual(29, testItem.SellIn);
             Assert.AreEqual(14, testItem.Quality);
         }
+
+        [Test]
+        public void testSellDatePassedDecrease()
+        {
+            Item testItem = new Item { Name = "foo", SellIn = 0, Quality = 15 };
+            IList<Item> Items = new List<Item> { testItem };
+            GildedRose app = new GildedRose(Items);
+
+            app.UpdateQuality();
+            Assert.AreEqual(-1, testItem.SellIn);
+            Assert.AreEqual(13, testItem.Quality);
+        }
     }
 }
